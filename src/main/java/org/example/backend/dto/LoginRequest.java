097487f65b59215942b392aebdb9f4e31c6ded15
danielvu052 <danielvu052@gmail.com>
2026-05-22 13:14:14 +0700

    Fix: fix_message

--- src/main/java/org/example/backend/dto/LoginRequest.java
+++ src/main/java/org/example/backend/dto/LoginRequest.java
@@ -3,10 +3,10 @@ package org.example.backend.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-        @NotBlank(message = "Email oder Username ist erforderlich")
+        @NotBlank(message = "Email or Username is required")
         String usernameOrEmail,
 
-        @NotBlank(message = "Passwort ist erforderlich")
+        @NotBlank(message = "Password is required")
         String password
 ) {
 }
